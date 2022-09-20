d7be1c0317191f21de0fe09a936cb924f29ff037
Lieu <shck1010@naver.com>
2022-09-21 07:55:34 +0900

    Generate API server spring-boot app

---
+++ Server_API/src/main/java/com/memoMainApiServer/app/MemoAppApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.memoMainApiServer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoAppApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoAppApiServerApplication.class, args);
+	}
+
+}
