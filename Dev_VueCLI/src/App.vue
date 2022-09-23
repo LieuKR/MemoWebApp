dd154a4ec6f90376e0762cd8ac7181834200a255
Lieu <shck1010@naver.com>
2022-09-23 09:09:42 +0900

    로그인 창 틀 완성

--- Dev_VueCLI/src/App.vue
+++ Dev_VueCLI/src/App.vue
@@ -23,5 +23,55 @@ body
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
+</style>
+
+<style lang="stylus">
+.request-box
+  display flex
+  flex-direction column
+  width calc(100% - 40px)
+  max-width 450px
+  margin 25px auto
+  background-color #262e3a
+  border-radius 8px
+  overflow hidden
+.request-box .header
+  padding 16px 16px 10px
+.request-box .body
+  padding 16px
+.request-box .header .title
+  font-size 1.1rem
+  font-weight 600
+.request-box .inputbox
+  margin-bottom 15px
+.request-box .input-text label
+  display: block;
+  font-size 14px
+  margin-bottom 7px
+  font-weight 600
+.request-box .input-text input
+  overflow hidden
+  border-radius 6px
+  color-scheme light
+  font-size 14px
+  background-color #000
+  color #fff
+  width -webkit-fill-available
+  height 40px
+  line-height 40px
+  border-color transparent
+  border-style solid
+  border-width 0 2px
+  padding 0px 10px
+.request-box .submitbox
+  overflow: hidden;
+  border-radius: 6px;
+  color-scheme: light;
+  font-size: 14px;
+  background-color: #5a5aec;
+  color: #fff;
+  width: -webkit-fill-available;
+  height: 40px;
+  border-color: transparent;
+  padding: 0px 10px;
 </style>
--- Dev_VueCLI/src/components/LoginView.vue
+++ Dev_VueCLI/src/components/LoginView.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="LoginView">
-    dafg
+  <div class="request-box">
+    <div class="header">
+      <span class="title">로그인</span>
+    </div>
+    <div class="body">
+      <div class="inputbox input-text">
+        <label for="input-email">이메일</label>
+        <div>
+          <input id="input-email" type="email" name="email" autocomplete="email" maxlength="50">
+        </div>
+      </div>
+      <div class="inputbox input-text">
+        <label for="input-password">비밀번호</label>
+        <div>
+          <input id="input-password" type="password" name="password" maxlength="50">
+        </div>
+      </div>
+      <button class="submitbox">로그인</button>
+    </div>
   </div>
 </template>
 
